2bbf3a783143119dcee9b713bcb8787e257246c2
mvc_ydb_admin <1609059419@qq.com>
2024-04-06 13:39:23 +0800

    feat: 1111

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -24,19 +24,7 @@ const config: Config = {
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en", "zh"],
-    localeConfigs: {
-      en: {
-        htmlLang: "en-GB",
-      },
-      zh: {
-        htmlLang: "zh-Hans",
-        direction: "ltr",
-      },
-    },
-  },
+
   plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
 
   presets: [
@@ -86,10 +74,6 @@ const config: Config = {
           label: "Tutorial",
         },
         { to: "/blog", label: "Blog", position: "left" },
-        {
-          type: "localeDropdown",
-          position: "right",
-        },
         {
           href: "https://github.com/facebook/docusaurus",
           label: "GitHub",
